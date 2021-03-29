77a936a13fbdb493ce090006d8ad01762d144e2b
Angel Sinn <angelkysinn@gmail.com>
2021-03-29 20:16:10 +0000

    Added logic to switch players & declare winner and refactored code

--- game.rb
+++ game.rb
@@ -3,13 +3,19 @@ class Game
   def initialize()
     @player_1 = Player.new("Player 1")
     @player_2 = Player.new("Player 2")
-    @current_player = @player_1.name
+    @current_player = @player_1
+  end
+
+  def game_intro
+    puts "========================================"
+    puts " *** Welcome to the Ruby Math Game *** "
+    puts "========================================"
   end
 
   def ask_question(current_player)
     @num1 = rand(1..20)
     @num2 = rand(1..20)
-    puts "#{@current_player}: What does #{@num1} plus #{@num2} equal?"
+    puts "#{@current_player.name}: What does #{@num1} plus #{@num2} equal?"
   end
 
   def verify_answer(input)
@@ -17,26 +23,49 @@ class Game
   end
 
   def show_score
-      puts "Current Stats - #{@player_1.name}: #{@player_1.lives}/3 vs #{@player_2.name}: #{@player_2.lives}/3"
+      puts "Stats - P1: #{@player_1.lives}/3 vs P2: #{@player_2.lives}/3"
   end
 
-  def start
-    puts "----New Game----"
-    show_score
-    puts "#{@current_player} will go first!"
-    ask_question(@current_player)
-    print "> "
-    input = gets.chomp().to_i
-    if verify_answer(input)
-      puts "YES! You are correct"
-    else 
-      puts "Seriously? No!"
-      @current_player.lose_life
+  def switch_player(current_player)
+    if current_player == @player_1
+      @current_player = @player_2
+    else
+      @current_player = @player_1
     end
   end
 
+  def declare_winner
+    if @player_1.lives > @player_2.lives
+      puts "=============================================="
+      puts "*** Player 1 wins with #{@player_1.lives}/3 lives remaining ***"
+      puts "=============================================="
+    else
+      puts "=============================================="
+      puts "*** Player 2 wins with #{@player_2.lives}/3 lives remaining ***"
+      puts "=============================================="
+    end
+  end
 
+  def start
+  game_intro
+    while @player_1.lives > 0 && @player_2.lives > 0 do
+      puts "--------------- New Turn ---------------"
+      puts "#{@current_player.name}'s turn!"
+      ask_question(@current_player)
+      print "> "
+      input = gets.chomp().to_i
 
-
+      if verify_answer(input)
+        puts "YES! You are correct!"
+        show_score
+      else 
+        puts "Seriously? No!"
+        @current_player.lose_life
+        show_score
+        switch_player(@current_player)
+      end
+    end
+    declare_winner
+  end
 end
     
--- player.rb
+++ player.rb
@@ -10,5 +10,4 @@ class Player
   def lose_life
       @lives -= 1
   end
-
 end
